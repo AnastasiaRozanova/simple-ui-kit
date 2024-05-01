46672236a8cd9ee454601c7726f469341ba6901f
anastasiiarozanova <rozanovaanastasia@redcollar.ru>
2024-05-01 19:15:56 +0300

    feat: add ui-kit

---
+++ src/shared/ui/atoms/button/index.tsx
@@ -0,0 +1,10 @@
+import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+import './styles.css';
+
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & PropsWithChildren & {
+	theme?: 'primary' | 'secondary' | 'danger';
+};
+
+export const Button = ({ children, theme = 'primary', ...props }: ButtonProps) => (
+		<button className={theme} {...props}>{children}</button>
+);
