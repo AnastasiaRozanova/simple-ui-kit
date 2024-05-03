5afc0d5fb572820ff37eae6f399565f047de11a8
anastasiiarozanova <rozanovaanastasia@redcollar.ru>
2024-05-03 13:39:49 +0300

    fix: styles

--- src/shared/ui/atoms/button/index.tsx
+++ src/shared/ui/atoms/button/index.tsx
@@ -3,9 +3,10 @@ import './styles.css';
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & PropsWithChildren & {
 	theme?: 'primary' | 'secondary' | 'danger';
+	size?: 'md' | 'lg';
 	className?: string;
 };
 
-export const Button = ({ children, theme = 'primary', className, ...props }: ButtonProps) => (
-		<button className={`button ${theme} ${className}`} {...props}>{children}</button>
+export const Button = ({ children, theme = 'primary', size = 'md', className, ...props }: ButtonProps) => (
+		<button className={`button ${theme} ${size} ${className}`} {...props}>{children}</button>
 );
