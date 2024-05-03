b750ba017258ac0d5052dfd4a0f4f631f143ccd1
anastasiiarozanova <rozanovaanastasia@redcollar.ru>
2024-05-03 13:36:16 +0300

    fix: package version

--- src/shared/ui/atoms/button/index.tsx
+++ src/shared/ui/atoms/button/index.tsx
@@ -3,8 +3,9 @@ import './styles.css';
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & PropsWithChildren & {
 	theme?: 'primary' | 'secondary' | 'danger';
+	className?: string;
 };
 
-export const Button = ({ children, theme = 'primary', ...props }: ButtonProps) => (
-		<button className={`button ${theme}`} {...props}>{children}</button>
+export const Button = ({ children, theme = 'primary', className, ...props }: ButtonProps) => (
+		<button className={`button ${theme} ${className}`} {...props}>{children}</button>
 );
