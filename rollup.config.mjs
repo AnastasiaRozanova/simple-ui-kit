2d94a1633a2998967306d0582f0c6eb8beba0c5b
anastasiiarozanova <rozanovaanastasia@redcollar.ru>
2024-05-03 12:55:45 +0300

    feat: config changes

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,15 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import packageJson from './package.json' assert { type: 'json' };
 
 export default [
   {
-    input: 'src/index.ts',
+    input: 'src/index.ts', // файл для сборки
     output: [
       {
         file: packageJson.main,
@@ -20,9 +22,12 @@ export default [
         sourcemap: true,
       },
     ],
+    // обеспечивают корректную работу и отсутвие ошибок
     plugins: [
-      resolve(),
-      commonjs(),
+      peerDepsExternal(), // те депсы, которые должны быть установлены у пользователей этого проекта
+      resolve({ extensions: ['.tsx', '.ts'] }), // чтобы сборщик понимал, что собирать
+      commonjs(), // сборка в формате commonjs
+      babel({ exclude: './node_modules/**' }), // компиляция js из es6 в es5
       typescript({ tsconfig: './tsconfig.json' }),
       postcss({ extensions: ['.css'], inject: true, extract: false }),
     ],
